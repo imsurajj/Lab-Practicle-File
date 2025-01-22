edada60375e29c6325ea5e3d91ec762f4414a193
Suraj <iamsurajbro946@gmail.com>
2025-01-22 22:35:05 +0530

    OUTPUT: Addition/Loops/HW/Christmas Patterns

--- JAVA/Loops.java
+++ JAVA/Loops.java
@@ -2,7 +2,7 @@
 class Loops{
     public static void main(String[] args){
         for(int i = 1; i <= 10; i++){
-            System.out.println("i");
+            System.out.print("i");
         }
     }
 }
