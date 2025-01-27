2f66f02b344c8c877438f12e9818ef2570a4e107
Suraj <iamsurajbro946@gmail.com>
2025-01-27 19:39:20 +0530

    Semester Wise Arranges

--- JAVA/Addition.java
+++
@@ -1,9 +0,0 @@
-// Addition using JAVA
-class Addition{
-    public static void main(String[] args){
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        System.out.println("Addition of " + a + " and " + b + " is " + c);
-    }
-}
--- JAVA/Loops.java
+++
@@ -1,8 +0,0 @@
-// Loops in Java
-class Loops{
-    public static void main(String[] args){
-        for(int i = 1; i <= 10; i++){
-            System.out.print("i");
-        }
-    }
-}
